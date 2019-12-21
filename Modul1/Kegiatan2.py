1cef6f88bd12b0549fdab7e9a849aa5bbedd544e
Kinotosi <59110259+Kinotosi@users.noreply.github.com>
2019-12-21 15:30:39 +0700

    Create Kegiatan2.py

---
+++ Modul1/Kegiatan2.py
@@ -0,0 +1,11 @@
+## Program Akun
+## Dibuat oleh Arga L200180114
+
+Nama = 'Arga Dwi Ardinata'
+TanggalLahir = '15 September 1997'
+print('Menampilkan nama singkat')
+print(Nama[0] + '. ' + Nama[5] + '. ' + Nama[9:])
+print('Menampilkan username')
+print(Nama[0] + TanggalLahir[0:2] + TanggalLahir[13:])
+print('Menampilkan Password')
+print(Nama[0:3] + '123')
