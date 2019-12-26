bbd71e410b8b68490b7178e01761afcba58c16bd
Kinotosi <59110259+Kinotosi@users.noreply.github.com>
2019-12-26 19:27:07 +0700

    Create Kegiatan1.py

---
+++ Modul 6/Kegiatan1.py
@@ -0,0 +1,12 @@
+## Program Identitas diri. Dibuat oleh Arga L200180114
+
+Nama = 'Arga Dwi Ardinata'
+Fakultas = 'Komunikasi dan Informatika'
+Prodi = 'Informatika'
+Alamat = 'Ngawi'
+Tanggal_lahir = '23 November 1997'
+Hobi = 'Rebahan'
+Motto = 'Bumbu penyedap'
+Tinggi = '188cm'
+Berat = '80kg'
+Cita_cita = 'Pengangguran sukses'
