b6479707c2cd08a90457da9d14c9121e98cfa2bd
Kinotosi <59110259+Kinotosi@users.noreply.github.com>
2019-12-26 18:26:21 +0700

    Delete Kegiatan1.py

--- Modul7/Kegiatan1.py
+++
@@ -1,9 +0,0 @@
-print("""
-No  | Nama bangun       | Rumus Luas
-----|-------------------|-----------------
-1   | Segitiga          | L = 0.5 * a * t
-2   | Persegi           | L = s ** 2
-3   | Persegi Panjang   | L = p * l
-4   | Lingkaran         | L = pi * r ** 2
-5   | Jajaran genjang   | L = a * t
-""")
